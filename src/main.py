82e79354f4e92fa65b7af6b27ee442353f67aa75
henri123lemoine <henri123lemoine@gmail.com>
2023-02-06 20:52:21 -0500

    added main.py, do not run

---
+++ src/main.py
@@ -0,0 +1,13 @@
+from dataset import Dataset
+from semantic_search import AlignmentSearch
+
+from settings import DATA_PATH
+
+def main():
+    dataset = Dataset(DATA_PATH)
+    dataset.get_alignment_texts()
+    dataset.load_embeddings()
+    search_and_answer = AlignmentSearch(dataset)
+    question = "What is an agent?"
+    answer = search_and_answer.search_and_answer(question)
+    print(answer)
