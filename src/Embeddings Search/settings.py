50354b248751770bc26573126ac8de42a9d9a5ab
Thomas Lemoine <lemoine123thomas@gmail.com>
2023-03-15 04:51:00 -0400

    path stored as pathlib's Path object

--- src/Embeddings Search/settings.py
+++ src/Embeddings Search/settings.py
@@ -1,9 +1,12 @@
+from pathlib import Path
+
 EMBEDDING_MODEL = "text-embedding-ada-002"
 COMPLETIONS_MODEL = "text-davinci-003"
 
 LEN_EMBEDDINGS = 1536
 MAX_LEN_PROMPT = 8191
 
-PATH_TO_DATA = r"C:\Users\Henri\Documents\GitHub\AlignmentSearch\data\alignment_texts.jsonl" # Path to the dataset .jsonl file.
-PATH_TO_EMBEDDINGS = r"C:\Users\Henri\Documents\GitHub\AlignmentSearch\src\Embeddings Search\data\embeddings.npy" # Path to the saved embeddings (.npy) file.
-PATH_TO_DATASET = r"C:\Users\Henri\Documents\GitHub\AlignmentSearch\src\Embeddings Search\data\dataset.pkl" # Path to the saved dataset (.pkl) file.
+project_path = Path(__file__).parent.parent.parent
+PATH_TO_DATA = project_path / "data" / "alignment_texts.jsonl" # Path to the dataset .jsonl file.
+PATH_TO_EMBEDDINGS = project_path / "src" / "Embeddings Search" / "data" / "embeddings.npy" # Path to the saved embeddings (.npy) file.
+PATH_TO_DATA = project_path / "src" / "Embeddings Search" / "data" / "dataset.pkl" # Path to the saved dataset (.pkl) file.
