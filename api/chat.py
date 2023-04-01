26f7d4411e64866d5f959c993967859627c6a613
Fraser <tomatoesneverlie@gmail.com>
2023-03-31 22:57:00 -0400

    get chat slightly more compiling

--- api/chat.py
+++ api/chat.py
@@ -19,26 +19,34 @@ TRUNCATE_CONTEXT_LEN = 1500
 TRUNCATE_HISTORY_LEN = 500
 MAX_RESPONSE_LEN = 900
 
-# -------------------------------- prompt code --------------------------------
+# --------------------------------- prompt code --------------------------------
 
 def limit_tokens(text: str, max_tokens: int, encoding_name: str = "cl100k_base") -> str:
     encoding = tiktoken.get_encoding(encoding_name)
     tokens = encoding.encode(text)[:max_tokens]
     return encoding.decode(tokens)
 
-def construct_prompt(query: str, history: List[Dict[str, str]], context: List[Block], encoding_name: str = "cl100k_base") -> List[Dict[str, str]]:
+def construct_prompt(query: str, history: List[Dict[str, str]], context: List[Block], encoding_name: str = "cl100k_base"):
+    # History takes the format: history=[
+    #     {"role": "system", "content": "You are a helpful assistant."},
+    #     {"role": "user", "content": "Who won the world series in 2020?"},
+    #     {"role": "assistant", "content": "The Los Angeles Dodgers won the World Series in 2020."},
+    #     {"role": "user", "content": "Where was it played?"}
+    #     {"role": "assistant", "content": "Los Angeles, California."}
+    # ]
+
     # Encoder to count tokens
     enc = tiktoken.get_encoding(encoding_name)
     total_tokens = 0
     
     prompt = []
     
-    system_prompt = "You are a helpful assistant knowledgeable about AI Alignment. You are provided with a question and a set of sources. Your job is to answer the question using the sources, and cite the sources you use."
+    system_prompt = "You are a helpful assistant knowledgeable about AI Alignment and Saftey."
     total_tokens += len(enc.encode(system_prompt))
     
     # Get past user queries
-    past_user_queries = "\n".join([message["content"] for message in history if message["role"] == "user"][-5:-1])
-    past_queries_prompt = f"My previous queries were:\n{past_user_queries}"
+    past_user_queries = "\nQ: ".join([message["content"] for message in history if message["role"] == "user"][-5:])
+    past_user_queries = f"My previous queries in our conversation have been:\n" + past_user_queries
     
     # Instruction prompt
     instruction_context_query_prompt = \
@@ -48,6 +56,7 @@ def construct_prompt(query: str, history: List[Dict[str, str]], context: List[Bl
 
     # Context from top-k blocks
     context_prompt = ""
+
     for i, block in enumerate(context):
         context_prompt += f"[{chr(ord('a') + i)}] {block.title} - {block.author} - {block.date}\n\n{block.text}\n\n\n"
     context_prompt = context_prompt[:-2] # trim last two newlines
@@ -57,14 +66,11 @@ def construct_prompt(query: str, history: List[Dict[str, str]], context: List[Bl
     # Question prompt
     question_prompt = f"In your answer, please cite any claims you make back to each source " \
                     f"using the format: [a], [b], etc. If you use multiple sources to make a claim " \
-                    f"cite all of them. For example: \"AGI is concerning [c, d, e].\"" \
-                    f"" \
-                    f"" \
-                    f"Q: {query}"
+                    f"cite all of them. For example: \"AGI is concerning [c, d, e].\"\n\nQ: " + query
 
     instruction_context_query_prompt = f"{instruction_context_query_prompt}\n\n{context_prompt}\n\n{question_prompt}"
     
-    total_tokens += len(enc.encode(past_queries_prompt))
+    total_tokens += len(enc.encode(past_user_queries))
     total_tokens += len(enc.encode(history[-2]["content"])) if (len(history) >= 2) else 0
     total_tokens += len(enc.encode(history[-1]["content"])) if (len(history) >= 1) else 0
     total_tokens += len(enc.encode(instruction_context_query_prompt))
@@ -77,7 +83,7 @@ def construct_prompt(query: str, history: List[Dict[str, str]], context: List[Bl
         prompt[-1]["content"] = f"{last_assistant_reply_trunctated}"
     
     prompt.append({"role": "system", "content": system_prompt})
-    prompt.append({"role": "user", "content": past_queries_prompt})
+    prompt.append({"role": "user", "content": past_user_queries})
     prompt.extend(history[-2:])
     prompt.append({"role": "user", "content": instruction_context_query_prompt})
     
@@ -96,7 +102,7 @@ def normal_completion(prompt: List[Dict[str, str]], max_tokens_completion: int) 
         print(e)
         return "I'm sorry, I failed to process your query. Please try again. If the problem persists, please contact the administrator."
 
-
+# returns either (True, reply string, embeddings) or (False, error message string, None)
 def talk_to_robot(dataset_dict, query: str, history: List[Dict[str, str]] = [], k: int = 10):
 
     # 1. Find the most relevant blocks from the Alignment Research Dataset
@@ -104,18 +110,9 @@ def talk_to_robot(dataset_dict, query: str, history: List[Dict[str, str]] = [], 
     
     # 2. Generate a prompt for the ChatCompletions API
     prompt, max_tokens_completion = construct_prompt(query, history, top_k_blocks)
+
+    # if we were to error out, return something like this
+    # return (False, "Example error message", None)
     
     # 3. Answer the user query
-    return (normal_completion(prompt, max_tokens_completion), top_k_blocks)
-
-
-if __name__ == "__main__":
-    import config
-    openai.api_key = config.OPENAI_API_KEY
-    
-    completion = openai.ChatCompletion.create(
-        model=COMPLETIONS_MODEL,
-        messages=[
-            {"role": "system", "content": "?" * 8 * 2045},
-        ]
-    )
+    return (True, normal_completion(prompt, max_tokens_completion), top_k_blocks)
