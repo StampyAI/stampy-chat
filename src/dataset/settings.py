0c97ea61c50687c8cf167309eee2d0efa49ec309
henri123lemoine <henri123lemoine@gmail.com>
2023-06-27 01:46:19 -0400

    Added settings file

---
+++ src/dataset/settings.py
@@ -0,0 +1,6 @@
+
+### PINECONE ###
+PINECONE_INDEX_NAME = "stampy-chat-embeddings-test"
+PINECONE_VALUES_DIMS = 1536
+PINECONE_METRIC = "cosine"
+PINECONE_METADATA_ENTRIES = ["entry_id", "source", "title", "authors", "text"]
