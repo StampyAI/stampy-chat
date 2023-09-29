844b98b405c10b8b20d06841e58331087a5d5a42
Daniel O'Connell <github@ahiru.pl>
2023-09-29 20:51:50 +0200

    chat.py tests

--- api/src/stampy_chat/chat.py
+++ api/src/stampy_chat/chat.py
@@ -29,26 +29,104 @@ CONTEXT_FRACTION = 0.5  # the (approximate) fraction of num_tokens to use for co
 
 ENCODER = tiktoken.get_encoding("cl100k_base")
 
+SOURCE_PROMPT = (
+    "You are a helpful assistant knowledgeable about AI Alignment and Safety. "
+    "Please give a clear and coherent answer to the user's questions.(written after \"Q:\") "
+    "using the following sources. Each source is labeled with a letter. Feel free to "
+    "use the sources in any order, and try to use multiple sources in your answers.\n\n"
+)
+SOURCE_PROMPT_SUFFIX = (
+    "\n\n"
+    "Before the question (\"Q: \"), there will be a history of previous questions and answers. "
+    "These sources only apply to the last question. Any sources used in previous answers "
+    "are invalid."
+)
+
+QUESTION_PROMPT = (
+    "In your answer, please cite any claims you make back to each source "
+    "using the format: [a], [b], etc. If you use multiple sources to make a claim "
+    "cite all of them. For example: \"AGI is concerning [c, d, e].\"\n\n"
+)
+PROMPT_MODES = {
+    'default': "",
+    "concise": (
+        "Answer very concisely, getting to the crux of the matter in as "
+        "few words as possible. Limit your answer to 1-2 sentences.\n\n"
+    ),
+    "rookie": (
+        "This user is new to the field of AI Alignment and Safety - don't "
+        "assume they know any technical terms or jargon. Still give a complete answer "
+        "without patronizing the user, but take any extra time needed to "
+        "explain new concepts or to illustrate your answer with examples. "
+        "Put extra effort into explaining the intuition behind concepts "
+        "rather than just giving a formal definition.\n\n"
+    ),
+}
+
 # --------------------------------- prompt code --------------------------------
 
 
 
 # limit a string to a certain number of tokens
 def cap(text: str, max_tokens: int) -> str:
-
-    if max_tokens <= 0: return "..."
+    if max_tokens <= 0:
+        return "..."
 
     encoded_text = ENCODER.encode(text)
 
-    if len(encoded_text) <= max_tokens: return text
-    else: return ENCODER.decode(encoded_text[:max_tokens]) + " ..."
+    if len(encoded_text) <= max_tokens:
+        return text
+    return ENCODER.decode(encoded_text[:max_tokens]) + " ..."
 
 
 Prompt = List[Dict[str, str]]
 
-def construct_prompt(query: str, mode: str, history: Prompt, context: List[Block]) -> Prompt:
 
+def prompt_context(source_prompt: str, context: List[Block], max_tokens: int) -> str:
+    token_count = len(ENCODER.encode(source_prompt))
+
+    # Context from top-k blocks
+    for i, block in enumerate(context):
+        block_str = f"[{chr(ord('a') + i)}] {block.title} - {','.join(block.authors)} - {block.date}\n{block.text}\n\n"
+        block_tc = len(ENCODER.encode(block_str))
+
+        if token_count + block_tc > max_tokens:
+            source_prompt += cap(block_str, max_tokens - token_count)
+            break
+        else:
+            source_prompt += block_str
+            token_count += block_tc
+    return source_prompt.strip()
+
+
+def prompt_history(history: Prompt, max_tokens: int, n_items=10) -> Prompt:
+    token_count = 0
     prompt = []
+
+    # Get the n_items last messages, starting from the last one. This is because it's assumed
+    # that more recent messages are more important. The `-1` is because of how slicing works
+    messages = history[:-n_items - 1:-1]
+    for message in messages:
+        if message["role"] == "user":
+            prompt.append({"role": "user", "content": "Q: " + message["content"]})
+            token_count += len(ENCODER.encode("Q: " + message["content"]))
+        else:
+            content = message["content"]
+            # censor all source letters into [x]
+            content = re.sub(r"\[[0-9]+\]", "[x]", content)
+            content = cap(content, max_tokens - token_count)
+
+            prompt.append({"role": "assistant", "content": content})
+            token_count += len(ENCODER.encode(content))
+
+        if token_count > max_tokens:
+            break
+    return prompt[::-1]
+
+
+def construct_prompt(query: str, mode: str, history: Prompt, context: List[Block]) -> Prompt:
+    if mode not in PROMPT_MODES:
+        raise ValueError("Invalid mode: " + mode)
 
     # History takes the format: history=[
     #     {"role": "user", "content": "Die monster. You don’t belong in this world!"},
@@ -59,87 +137,23 @@ def construct_prompt(query: str, mode: str, history: Prompt, context: List[Block
     #     {"role": "assistant", "content": "What is a man? A miserable little pile of secrets. But enough talk... Have at you!"},
     # ]
 
-    source_prompt = "You are a helpful assistant knowledgeable about AI Alignment and Safety. " \
-        "Please give a clear and coherent answer to the user's questions.(written after \"Q:\") " \
-        "using the following sources. Each source is labeled with a letter. Feel free to " \
-        "use the sources in any order, and try to use multiple sources in your answers.\n\n"
-
-    token_count = len(ENCODER.encode(source_prompt))
-
     # Context from top-k blocks
-    for i, block in enumerate(context):
-        block_str = f"[{chr(ord('a') + i)}] {block.title} - {','.join(block.authors)} - {block.date}\n{block.text}\n\n"
-        block_tc = len(ENCODER.encode(block_str))
-
-        if token_count + block_tc > int(NUM_TOKENS * CONTEXT_FRACTION):
-            source_prompt += cap(block_str, int(NUM_TOKENS * CONTEXT_FRACTION) - token_count)
-            break
-        else:
-            source_prompt += block_str
-            token_count += block_tc
-
-    source_prompt = source_prompt.strip();
-    if len(history) > 0:
-        source_prompt += "\n\n"\
-            "Before the question (\"Q: \"), there will be a history of previous questions and answers. " \
-            "These sources only apply to the last question. Any sources used in previous answers " \
-            "are invalid."
-
-    prompt.append({"role": "system", "content": source_prompt.strip()})
-
+    source_prompt = prompt_context(SOURCE_PROMPT, context, int(NUM_TOKENS * CONTEXT_FRACTION))
+    if history:
+        source_prompt += SOURCE_PROMPT_SUFFIX
+    source_prompt = [{"role": "system", "content": source_prompt.strip()}]
 
     # Write a version of the last 10 messages into history, cutting things off when we hit the token limit.
-    token_count = 0
-    history_trnc = []
-    for message in history[:-10:-1]:
-        if message["role"] == "user":
-            history_trnc.append({"role": "user", "content": "Q: " + message["content"]})
-            token_count += len(ENCODER.encode("Q: " + message["content"]))
-        else:
-            content = cap(message["content"], int(NUM_TOKENS * HISTORY_FRACTION) - token_count)
+    history_prompt = prompt_history(history, int(NUM_TOKENS * HISTORY_FRACTION))
+    question_prompt = [{"role": "user", "content": QUESTION_PROMPT + PROMPT_MODES[mode] + "Q: " + query}]
 
-            # censor all source letters into [x]
-            content = re.sub(r"\[[0-9]+\]", "[x]", content)
-
-            history_trnc.append({"role": "assistant", "content": content})
-            token_count += len(ENCODER.encode(content))
-
-        if token_count > int(NUM_TOKENS * HISTORY_FRACTION):
-            break
-
-    prompt.extend(history_trnc[::-1])
-
-
-    question_prompt = "In your answer, please cite any claims you make back to each source " \
-                      "using the format: [a], [b], etc. If you use multiple sources to make a claim " \
-                      "cite all of them. For example: \"AGI is concerning [c, d, e].\"\n\n"
-
-    if mode == "concise":
-        question_prompt += "Answer very concisely, getting to the crux of the matter in as " \
-                "few words as possible. Limit your answer to 1-2 sentences.\n\n"
-
-    elif mode == "rookie":
-        question_prompt += "This user is new to the field of AI Alignment and Safety - don't " \
-                "assume they know any technical terms or jargon. Still give a complete answer " \
-                "without patronizing the user, but take any extra time needed to " \
-                "explain new concepts or to illustrate your answer with examples. "\
-                "Put extra effort into explaining the intuition behind concepts " \
-                "rather than just giving a formal definition.\n\n"
-
-    elif mode != "default": raise ValueError("Invalid mode: " + mode)
-
-
-    question_prompt += "Q: " + query
-
-    prompt.append({"role": "user", "content": question_prompt})
-
-    return prompt
+    return source_prompt + history_prompt + question_prompt
 
 # ------------------------------- completion code -------------------------------
 
 def check_openai_moderation(prompt: Prompt, query: str):
     prompt_string = '\n\n'.join([message["content"] for message in prompt])
-    mod_res = openai.Moderation.create( input = [ query, prompt_string ])
+    mod_res = openai.Moderation.create(input=[query, prompt_string])
 
     if any(map(lambda x: x["flagged"], mod_res["results"])):
         logger.moderation_issue(query, prompt_string, mod_res)
@@ -153,7 +167,7 @@ def remaining_tokens(prompt: Prompt):
         len(ENCODER.encode(message["content"]) + ENCODER.encode(message["role"]))
         for message in prompt
     ])
-    return NUM_TOKENS - used_tokens - TOKENS_BUFFER
+    return max(0, NUM_TOKENS - used_tokens - TOKENS_BUFFER)
 
 
 def talk_to_robot_internal(index, query: str, mode: str, history: Prompt, k: int = STANDARD_K):
@@ -188,10 +202,9 @@ def talk_to_robot_internal(index, query: str, mode: str, history: Prompt, k: int
             temperature=0, # may or may not be a good idea
         ):
             res = chunk["choices"][0]["delta"]
-            if res is not None and res.get("content") is not None:
+            if res and res.get("content"):
                 response += res["content"]
                 yield {"state": "streaming", "content": res["content"]}
-
 
         t2 = time.time()
         logger.debug(f'Time to get response: {time.time() - t1:.2f}s')
@@ -218,9 +231,11 @@ def talk_to_robot_internal(index, query: str, mode: str, history: Prompt, k: int
         logger.error(e)
         yield {'state': 'error', 'error': str(e)}
 
+
 # convert talk_to_robot_internal from dict generator into json generator
 def talk_to_robot(index, query: str, mode: str, history: List[Dict[str, str]], k: int = STANDARD_K):
     yield from (json.dumps(block) for block in talk_to_robot_internal(index, query, mode, history, k))
+
 
 # wayyy simplified api
 def talk_to_robot_simple(index, query: str):
